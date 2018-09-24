366acc454c175c0e8144d0144c2d4463a5b5e170
Pelle Sundin <pellesundin@hotmail.com>
2018-09-24 22:39:57 +0200

    try fix fade

--- myscripts.js
+++ myscripts.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $('.fadein').each(function (i) {
 
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height()+200;
+            var bottom_of_window = $(window).scrollTop() + $(window).height+200();
 
             /* If the object is completely visible in the window, fade it */
             if (bottom_of_window > bottom_of_object) {
