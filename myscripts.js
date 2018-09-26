3cfb48f4de02df4cce1d84b9d6a2477b6d566624
Pelle Sundin <pellesundin@hotmail.com>
2018-09-26 11:34:47 +0200

    cleaned up code

--- myscripts.js
+++ myscripts.js
@@ -1,29 +1,51 @@
 $(document).ready(function () {
+    var $window = $(window),
+        $fadeIn = $('.fadein'),
+        $navFace = $('.navFade'),
+        $headerFade = $(".headerFade"),
+        $delayedFade = $(".delayedFade"),
+        fadeInSpeed = 1000,
+        slideDownSpeed = 1000,
+        navFaceDelay = 500,
+        navFaceDelayHide = 300;
 
     /* Every time the window is scrolled ... */
-    $(window).scroll(function () {
+    $window.scroll(function () {
 
         /* Check the location of each desired element */
-        $('.fadein').each(function (i) {
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height()+200;
+        $fadeIn.each(function () {
+            var offsetHeight = 200;
+            var $this = $(this);
+            var bottom_of_object = $this.offset().top + $this.outerHeight();
+            var bottom_of_window = $window.scrollTop() + ($window.height() + offsetHeight);
+            var windowCollideWithObject = (bottom_of_window > bottom_of_object);
 
             /* If the object is completely visible in the window, fade it */
-            if (bottom_of_window > bottom_of_object) {
-
-                $(this).animate({ 'opacity': '1' }, 700);
+            if (windowCollideWithObject) {
+                $this.animate({
+                    opacity: 1
+                }, 700);
             }
         });
     });
 
-    $('.navFade').stop(true).delay(500).fadeIn({
-        duration: 500,
-        queue: false
-    }).css('display', 'none').delay(300).slideDown(1000);
+    $navFace
+        .stop(true)
+        .delay(navFaceDelay)
+        .fadeIn({
+            duration: navFaceDelay,
+            queue: false
+        })
+        .css('display', 'none')
+        .delay(navFaceDelayHide)
+        .slideDown(slideDownSpeed);
 
-    $(".headerFade").hide().fadeIn(1000);
+    $headerFade
+        .hide()
+        .fadeIn(fadeInSpeed);
 
-    $(".delayedFade").hide().delay(2000).fadeIn(1000);
-
+    $delayedFade
+        .hide()
+        .delay(2000)
+        .fadeIn(fadeInSpeed);
 });
